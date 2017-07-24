2a92808e89b8ec4e4d8d04a6c9b06a6d6f8d38d4
Patrick Stockwell <stockwellpatrick@gmail.com>
2017-07-24 15:00:52 +1000

    add initial tests for api_request

---
+++ test/api_request.test.js
@@ -0,0 +1,21 @@
+const expect = require('chai').expect
+const get = require('../modules/api_request').getImageData
+
+let data = {}
+describe('api_request file', function() {
+    describe('The getImageData function', function() {
+        it('should execute the callback', function(done) {
+            function dummy(x) {
+                data = x
+                console.log(data)
+                done()
+            }
+            get('http://take-home-test.herokuapp.com/api/v1/works.xml', dummy)
+        })
+    })
+    describe('The data object', function() {
+        it('should exist', function() {
+            expect(data).to.include.all.keys('works');
+        })
+    })
+})

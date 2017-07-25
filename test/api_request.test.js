fbc9bcd79e2463bcf76b1fedd68c25918c8a6755
Patrick Stockwell <stockwellpatrick@gmail.com>
2017-07-25 11:24:02 +1000

    add comments, update folder name, add 10sec timeout for tests

--- test/api_request.test.js
+++ test/api_request.test.js
@@ -1,8 +1,11 @@
 const expect = require('chai').expect
-const get = require('../modules/api_request')
+const get = require('../lib/api_request')
 
 let data = {}
 describe('api_request file', function() {
+    // add a 10sec timeout to override the 2sec defaults
+    // allow time for the async call to be made on poor connections
+    this.timeout(10000);
     describe('The getImageData function', function() {
         it('should execute the callback', function(done) {
             function dummy(x) {

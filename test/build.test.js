1b25669fc73a983a4a5c060ce97ab30ed299d090
Patrick Stockwell <stockwellpatrick@gmail.com>
2017-07-26 21:08:24 +1000

    add tests for garbage input

--- test/build.test.js
+++ test/build.test.js
@@ -13,15 +13,35 @@ describe('the build module', function() {
         it('should contain a heading with index', function() {
             expect(string.indexOf('<h1>Index Page</h1>') > -1).to.be.true
         })
+        it('should still return a string when the data is garbage', function() {
+            const stillSomeHTMLString = build.mainIndex('a;sdlkfja;ds02934857   ....... asdf', '_)(*Plj;asdf)')
+            expect(stillSomeHTMLString).to.be.a('string')
+        })
     })
     describe('the models function', function() {
         it('should return an array when no directory argument is given', function() {
             expect(build.models(parsedData)).to.be.an('array')
         })
+        it('should return an array when the data is garbage', function() {
+            const someArray = build.models('lsk(874#@#% ..  |\][])')
+            expect(someArray).to.be.a('array')
+        })
+        it('should return an empty array when the data is garbage', function() {
+            const someArray = build.models('lsk(874#@#% ..  |\][])')
+            expect(someArray).to.have.lengthOf(0)
+        })
     })
     describe('the makes function', function() {
         it('should return an array when no directory argument is given', function() {
             expect(build.makes(parsedData)).to.be.an('array')
+        })
+        it('should return an array when the data is garbage', function() {
+            const someArray = build.makes('lsk(874#@#% ..  |\][])')
+            expect(someArray).to.be.a('array')
+        })
+        it('should return an empty array when the data is garbage', function() {
+            const someArray = build.makes('lsk(874#@#% ..  |\][])')
+            expect(someArray).to.have.lengthOf(0)
         })
     })
 })
